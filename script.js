df4cddc727e387b58a4558e5a3a3c33e8a9f066d
Sri Dinu <sridinu03@gmail.com>
2023-10-12 11:32:21 +0530

    logo and loader added

--- script.js
+++ script.js
@@ -3,6 +3,16 @@
 // });
 var isLogin = false;
 
+function loader() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loader").style.visibility = "visible";
+    } else {
+        document.querySelector("#loader").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+}
+
 function attemptQuiz() {
     var quiz = document.getElementById("quiz");
     var post = document.getElementById("scrollPost");
